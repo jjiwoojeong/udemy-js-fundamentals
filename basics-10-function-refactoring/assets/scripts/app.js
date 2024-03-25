0840a05ef65fb5ea83175c16bf415e78a112443b
jjiwoojeong <54689431+jjiwoojeong@users.noreply.github.com>
2024-03-25 18:55:19 -0300

    basics-12-comments

--- basics-10-function-refactoring/assets/scripts/app.js
+++ basics-10-function-refactoring/assets/scripts/app.js
@@ -1,10 +1,28 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  const calcDescription = `${currentResult} + ${userInput.value}`;
-  currentResult = currentResult + parseInt(userInput.value);
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, currentResult);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
